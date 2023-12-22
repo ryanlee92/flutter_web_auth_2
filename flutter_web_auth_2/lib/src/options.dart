1c4784ef50555f7530893b0070d14179dc387c72
Nico Mexis <nico.mexis@kabelmail.de>
2023-12-22 13:51:34 +0100

    Fix null-safe options (again)

--- flutter_web_auth_2/lib/src/options.dart
+++ flutter_web_auth_2/lib/src/options.dart
@@ -97,14 +97,18 @@ class FlutterWebAuth2Options {
   final bool silentAuth;
 
   const FlutterWebAuth2Options({
-    this.preferEphemeral = false,
+    bool? preferEphemeral,
     this.debugOrigin,
-    this.intentFlags = defaultIntentFlags,
+    int? intentFlags,
     this.windowName,
-    this.timeout = 5 * 60,
-    this.landingPageHtml = _defaultLandingPage,
-    this.silentAuth = false,
-  });
+    int? timeout,
+    String? landingPageHtml,
+    bool? silentAuth,
+  })  : preferEphemeral = preferEphemeral ?? false,
+        intentFlags = intentFlags ?? defaultIntentFlags,
+        timeout = timeout ?? 5 * 60,
+        landingPageHtml = landingPageHtml ?? _defaultLandingPage,
+        silentAuth = silentAuth ?? false;
 
   FlutterWebAuth2Options.fromJson(Map<String, dynamic> json)
       : this(
