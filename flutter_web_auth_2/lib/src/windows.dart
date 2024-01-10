c74f56e2db01c5f88f61642e3430b718bce645b0
Long Yang Paffrath <paffrath@yangnet.de>
2024-01-10 23:45:11 +0100

    Small changes

--- flutter_web_auth_2/lib/src/windows.dart
+++ flutter_web_auth_2/lib/src/windows.dart
@@ -5,6 +5,9 @@ import 'package:desktop_webview_window/desktop_webview_window.dart';
 import 'package:flutter_web_auth_2_platform_interface/flutter_web_auth_2_platform_interface.dart';
 
 class FlutterWebAuth2WindowsPlugin extends FlutterWebAuth2Platform {
+  bool authenticated = false;
+  Webview? webview;
+
   static void registerWith() {
     FlutterWebAuth2Platform.instance = FlutterWebAuth2WindowsPlugin();
   }
@@ -16,25 +19,32 @@ class FlutterWebAuth2WindowsPlugin extends FlutterWebAuth2Platform {
     required Map<String, dynamic> options,
   }) async {
     if (!await WebviewWindow.isWebviewAvailable()) {
+      //Microsofts WebView2 must be installed for this to work
       throw StateError('Webview is not available');
     }
+    //Reset
+    authenticated = false;
+    webview?.close();
+
     final c = Completer<String>();
-    final webview = await WebviewWindow.create(
+
+    webview = await WebviewWindow.create(
       configuration: CreateConfiguration(
         windowHeight: 720,
         windowWidth: 1280,
-        title: 'easyroam authentication',
+        title: 'Authenticate',
         titleBarTopPadding: Platform.isMacOS ? 20 : 0,
       ),
     );
-    webview.addOnUrlRequestCallback((url) {
+    webview!.addOnUrlRequestCallback((url) {
       final uri = Uri.parse(url);
       if (uri.scheme == callbackUrlScheme) {
+        authenticated = true;
         c.complete(url);
-        webview.close();
+        webview?.close();
       }
     });
-    webview.launch(url);
+    webview!.launch(url);
     return c.future;
   }
 }
